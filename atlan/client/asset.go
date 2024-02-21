6f5d2a9c0c6b7daf92ada9bb3665c11765c26028
Karanjot Singh <drquark@duck.com>
2024-02-21 22:44:36 +0530

    Update Attributes in Asset fields
    Signed-off-by: Karanjot Singh <drquark@duck.com>

--- atlan/client/asset.go
+++ atlan/client/asset.go
@@ -6,67 +6,67 @@ import (
 	"fmt"
 )
 
-// Struct to represent assets for searching
+// SearchAssets Struct to represent assets for searching
 type SearchAssets struct {
 	Glossary *AtlasGlossary
 	Table    *AtlasTable
 	// Add other assets here
 }
 
-type Attributes struct {
-	TypeName            *KeywordTextField
+type AttributesFields struct {
+	TYPENAME            *KeywordTextField
 	GUID                *KeywordField
-	CreatedBy           *KeywordField
-	UpdatedBy           *KeywordField
-	Status              *KeywordField
-	AtlanTags           *KeywordTextField
-	PropagatedAtlanTags *KeywordTextField
-	AssignedTerms       *KeywordTextField
-	SuperTypeNames      *KeywordTextField
-	CreateTime          *NumericField
-	UpdateTime          *NumericField
-	QualifiedName       *KeywordTextField
+	CREATEDBY           *KeywordField
+	UPDATEDBY           *KeywordField
+	STATUS              *KeywordField
+	ATLANTAGS           *KeywordTextField
+	PROPOGATEDATLANTAGS *KeywordTextField
+	ASSIGNEDTERMS       *KeywordTextField
+	SUPERTYPENAMES      *KeywordTextField
+	CREATETIME          *NumericField
+	UPDATETIME          *NumericField
+	QUALIFIEDNAME       *KeywordTextField
 }
 
-type Asset struct {
-	*Attributes
-	Name                     *KeywordTextStemmedField
-	DislayName               *KeywordTextField
-	Description              *KeywordTextField
-	UserDescription          *KeywordTextField
-	TenetID                  *KeywordField
-	CetificateStatus         *KeywordTextField
-	CertificateStatusMessage *KeywordField
-	CertificateUpdatedBy     *NumericField
-	AnnouncementTitle        *KeywordField
-	AnnouncementMessage      *KeywordTextField
-	AnnouncementType         *KeywordField
-	AnnouncementUpdatedAt    *NumericField
-	AnnouncementUpdatedBy    *KeywordField
-	OwnerUsers               *KeywordTextField
-	AdminUsers               *KeywordField
-	ViewerUsers              *KeywordField
-	ViewerGroups             *KeywordField
-	ConnectorName            *KeywordTextField
-	ConnectionName           *KeywordTextField
+type AssetFields struct {
+	*AttributesFields
+	NAME                     *KeywordTextStemmedField
+	DISPLAYNAME              *KeywordTextField
+	DESCRIPTION              *KeywordTextField
+	USERDESCRIPTION          *KeywordTextField
+	TENETID                  *KeywordField
+	CERTIFICATESTATUS        *KeywordTextField
+	CERTIFICATESTATUSMESSAGE *KeywordField
+	CERTIFICATEUPDATEDBY     *NumericField
+	ANNOUNCEMENTTITLE        *KeywordField
+	ANNOUNCEMENTMESSAGE      *KeywordTextField
+	ANNOUNCEMENTTYPE         *KeywordField
+	ANNOUNCEMENTUPDATEDAT    *NumericField
+	ANNOUNCEMENTUPDATEDBY    *KeywordField
+	OWNERUSERS               *KeywordTextField
+	ADMINUSERS               *KeywordField
+	VIEWERUSERS              *KeywordField
+	VIEWERGROUPS             *KeywordField
+	CONNECTORNAME            *KeywordTextField
+	CONNECTIONNAME           *KeywordTextField
 }
 
 // AtlasGlossary represents the AtlasGlossary asset
 type AtlasGlossary struct {
-	Asset
+	AssetFields
 	AtlanObject
 	Entities []model.Glossary `json:"entities"`
 }
 
 type AtlasTable struct {
-	Attributes
+	AttributesFields
 }
 
 // NewSearchTable returns a new AtlasTable object for Searching
 func NewSearchTable() *AtlasTable {
 	return &AtlasTable{
-		Attributes: Attributes{
-			TypeName: NewKeywordTextField("typeName", "__typeName.keyword", "__typeName"),
+		AttributesFields: AttributesFields{
+			TYPENAME: NewKeywordTextField("typeName", "__typeName.keyword", "__typeName"),
 		},
 	}
 }
@@ -74,23 +74,23 @@ func NewSearchTable() *AtlasTable {
 // NewSearchGlossary returns a new AtlasGlossary object for Searching
 func NewSearchGlossary() *AtlasGlossary {
 	return &AtlasGlossary{
-		Asset: Asset{
-			Attributes: &Attributes{
-				TypeName:            NewKeywordTextField("typeName", "__typeName.keyword", "__typeName"),
+		AssetFields: AssetFields{
+			AttributesFields: &AttributesFields{
+				TYPENAME:            NewKeywordTextField("typeName", "__typeName.keyword", "__typeName"),
 				GUID:                NewKeywordField("guid", "__guid"),
-				CreatedBy:           NewKeywordField("createdBy", "__createdBy"),
-				UpdatedBy:           NewKeywordField("updatedBy", "__modifiedBy"),
-				Status:              NewKeywordField("status", "__state"),
-				AtlanTags:           NewKeywordTextField("classificationNames", "__traitNames", "__classificationsText"),
-				PropagatedAtlanTags: NewKeywordTextField("classificationNames", "__propagatedTraitNames", "__classificationsText"),
-				AssignedTerms:       NewKeywordTextField("meanings", "__meanings", "__meaningsText"),
-				SuperTypeNames:      NewKeywordTextField("typeName", "__superTypeNames.keyword", "__superTypeNames"),
-				CreateTime:          NewNumericField("createTime", "__timestamp"),
-				UpdateTime:          NewNumericField("updateTime", "__modificationTimestamp"),
-				QualifiedName:       NewKeywordTextField("qualifiedName", "qualifiedName", "qualifiedName.text"),
+				CREATEDBY:           NewKeywordField("createdBy", "__createdBy"),
+				UPDATEDBY:           NewKeywordField("updatedBy", "__modifiedBy"),
+				STATUS:              NewKeywordField("status", "__state"),
+				ATLANTAGS:           NewKeywordTextField("classificationNames", "__traitNames", "__classificationsText"),
+				PROPOGATEDATLANTAGS: NewKeywordTextField("classificationNames", "__propagatedTraitNames", "__classificationsText"),
+				ASSIGNEDTERMS:       NewKeywordTextField("meanings", "__meanings", "__meaningsText"),
+				SUPERTYPENAMES:      NewKeywordTextField("typeName", "__superTypeNames.keyword", "__superTypeNames"),
+				CREATETIME:          NewNumericField("createTime", "__timestamp"),
+				UPDATETIME:          NewNumericField("updateTime", "__modificationTimestamp"),
+				QUALIFIEDNAME:       NewKeywordTextField("qualifiedName", "qualifiedName", "qualifiedName.text"),
 			},
-			DislayName: NewKeywordTextField("displayName", "displayName.keyword", "displayName"),
-			Name:       NewKeywordTextStemmedField("name", "name.keyword", "name", "name"),
+			DISPLAYNAME: NewKeywordTextField("displayName", "displayName.keyword", "displayName"),
+			NAME:        NewKeywordTextStemmedField("name", "name.keyword", "name", "name"),
 		},
 	}
 }
