9e52d3a3cf1bab03d289b559739815c03b70598f
Karanjot Singh <drquark@duck.com>
2024-04-16 01:46:42 +0530

    Reformat glossary test
    Signed-off-by: Karanjot Singh <drquark@duck.com>

--- atlan/client/glossary_client_test.go
+++ atlan/client/glossary_client_test.go
@@ -13,8 +13,17 @@ func TestIntegrationGlossary(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping integration test")
 	}
+
 	LoggingEnabled = false
 	NewContext()
+
+	glossaryGUID := testCreateGlossary(t)
+	testUpdateGlossary(t, glossaryGUID)
+	testRetrieveGlossary(t, glossaryGUID)
+	testDeleteGlossary(t, glossaryGUID)
+}
+
+func testCreateGlossary(t *testing.T) string {
 	g := &AtlasGlossary{}
 	// Create Glossary
 	g.Creator(GlossaryName, atlan.AtlanIconAirplaneInFlight)
@@ -31,9 +40,12 @@ func TestIntegrationGlossary(t *testing.T) {
 	assert.Equal(t, GlossaryName, *assetone.Attributes.Name, "glossary name should match")
 	assert.Equal(t, *g.TypeName, assetone.TypeName, "glossary type should match")
 
-	// Update Glossary
-	glossaryQualifiedName := *assetone.Attributes.QualifiedName
-	glossaryGUID := assetone.Guid
+	return assetone.Guid
+}
+
+func testUpdateGlossary(t *testing.T, glossaryGUID string) {
+	g := &AtlasGlossary{}
+	glossaryQualifiedName := GlossaryName + "-qual"
 	DisplayName := "gsdk-test-update"
 	g.Updater(GlossaryName, glossaryQualifiedName, glossaryGUID)
 	g.DisplayName = &DisplayName
@@ -44,19 +56,23 @@ func TestIntegrationGlossary(t *testing.T) {
 	assert.NotNil(t, updateresponse, "fetched glossary should not be nil")
 	assert.Equal(t, 1, len(updateresponse.MutatedEntities.UPDATE), "number of glossaries updated should be 1")
 	assert.Equal(t, *g.DisplayName, *updateresponse.MutatedEntities.UPDATE[0].Attributes.DisplayText, "glossary display name should match")
+}
 
-	// Retrieve Glossary by Guid
+func testRetrieveGlossary(t *testing.T, glossaryGUID string) {
 	glossary, err := GetGlossaryByGuid(glossaryGUID)
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
 	assert.NotNil(t, glossary, "fetched glossary should be nil")
 	assert.Equal(t, glossaryGUID, *glossary.Guid, "glossary guid should match")
+}
 
-	// Delete Glossary (Hard)
-	deleteresponse, _ := PurgeByGuid([]string{glossaryGUID})
+func testDeleteGlossary(t *testing.T, glossaryGUID string) {
+	deleteresponse, err := PurgeByGuid([]string{glossaryGUID})
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 	assert.NotNil(t, deleteresponse, "fetched glossary should not be nil")
 	assert.Equal(t, 1, len(deleteresponse.MutatedEntities.DELETE), "number of glossaries deleted should be 1")
 	assert.Equal(t, glossaryGUID, deleteresponse.MutatedEntities.DELETE[0].Guid, "glossary guid should match")
-
 }
