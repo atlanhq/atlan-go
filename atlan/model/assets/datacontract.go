b70f0cc43d331c4ed84da06328e9ca719c719842
Karanjot Singh <drquark@duck.com>
2024-04-27 17:47:04 +0530

    Add datacontract typedef
    Signed-off-by: Karanjot Singh <drquark@duck.com>

--- atlan/model/assets/datacontract.go
+++ atlan/model/assets/datacontract.go
@@ -1,6 +1,21 @@
 package assets
 
 type DataContract struct {
+	Catalog
+	DataContractJson *string `json:"dataContractJson,omitempty"`
+	Version          *string `json:"dataContractVersion,omitempty"`
+	AssetGuid        *string `json:"dataContractAssetGuid,omitempty"`
+	// Relationships
+	LatestCertified         *[]Asset        `json:"dataContractLatestCertified,omitempty"`
+	ContractAssetCertified  *[]DataContract `json:"dataContractAssetCertified,omitempty"`
+	ContractLatest          *[]Asset        `json:"dataContractLatest,omitempty"`
+	ContractAssetLatest     *[]DataContract `json:"dataContractAssetLatest,omitempty"`
+	ContractPreviousVersion *[]DataContract `json:"dataContractPreviousVersion,omitempty"`
+	ContractNextVersion     *[]DataContract `json:"dataContractNextVersion,omitempty"`
+}
+
+/*
+type DataContract struct {
 	Asset
 	Name              *string            `json:"name,omitempty"`
 	QualifiedName     *string            `json:"qualifiedName,omitempty"`
@@ -18,3 +33,4 @@ type RelAttribute struct {
 	TypeName         *string           `json:"typeName,omitempty"`
 	UniqueAttributes *UniqueAttributes `json:"uniqueAttributes,omitempty"`
 }
+*/
